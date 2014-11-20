c032630d95048f22d1e4d1a6623a998b16546088
Allegra M Denton <allegramdenton@MEGA-MONSTER.local>
2014-11-20 00:34:07 -0500

    First commit

---
+++ dracula_json.js
@@ -0,0 +1,17 @@
+[
+    {
+        "sentence": "Listen to them, the children of the night. What music they make!"
+    },
+    {
+        "sentence": "There are darknesses in life and there are lights, and you are one of the lights, the light of all lights."
+    },
+    {
+        "sentence": "Remember my friend, that knowledge is stronger than memory, and we should not trust the weaker"
+    },
+    {
+        "sentence": "I am all in a sea of wonders. I doubt; I fear; I think strange things, which I dare not confess to my own soul. God keep me, if only for the sake of those dear to me!"
+    },
+    {
+        "sentence": "I want you to believe...to believe in things that you cannot."
+    }
+]
